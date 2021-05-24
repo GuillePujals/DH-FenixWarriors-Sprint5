4ce61b320a917fa7143ca59007e7cddfc573347b
GuillePujals <guille_pujals@yahoo.com.ar>
2021-05-24 12:22:37 -0300

    Guille: middleware authAdm

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const controladorproduct = require('../controller/productController');
 
+const authAdmMiddleware = require('../middlewares/authAdmMiddleware');
+
 //Configuración de multer para archivo de producto
 const multer = require('multer');
 const path = require('path');
@@ -24,19 +26,19 @@ router.get('/cart/', controladorproduct.carrito);
 
 //Ottonello - 19-5 - Revisar rutas sprint4
 //1. /products (GET) Listado de productos
-router.get('/',controladorproduct.list);
+router.get('/',authAdmMiddleware, controladorproduct.list);
 //2. 2. /products/create (GET) Formulario de creación de productos
-router.get('/create', controladorproduct.create);
+router.get('/create',authAdmMiddleware, controladorproduct.create);
 //3. /products/:id (GET) Detalle de un producto particular
-router.get('/:id', controladorproduct.detalleCrud);
+router.get('/:id',authAdmMiddleware, controladorproduct.detalleCrud);
 //4. /products (POST) Acción de creación (a donde se envía el formulario)
 router.post('/', upload.single('image'),validations, controladorproduct.store);
 //5. /products/:id/edit (GET) Formulario de edición de productos
-router.get('/:id/edit', controladorproduct.edit);
+router.get('/:id/edit',authAdmMiddleware, controladorproduct.edit);
 //6. /products/:id (PUT) Acción de edición (a donde se envía el formulario)
-router.put('/:id/edit', upload.single('image'),validations, controladorproduct.update);
+router.put('/:id/edit',authAdmMiddleware, upload.single('image'),validations, controladorproduct.update);
 //7. /products/:id (DELETE) Acción de borrado
-router.delete('/:id' , controladorproduct.delete );
+router.delete('/:id' ,authAdmMiddleware, controladorproduct.delete );
 
 
 
